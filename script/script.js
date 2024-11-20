7676649fad2183de7abf578372ec5ae7c7632893
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-11-20 11:50:00 +0100

    feat: animation fix: script.js template sintax error

--- script/script.js
+++ script/script.js
@@ -5,22 +5,27 @@ function generateCards() {
   axios
     .get(apiUrl)
     .then((response) => {
+
+      console.log(response);
+
       const data = response.data;
+
+      console.log(data);
 
       cardContainerElement.innerHTML = "";
 
       data.forEach((element) => {
         const template = `
-          <div class="card shadow">
-              <img class="pin" src="../img/pin.svg" alt="pin"/>
+          <div id="card-${element.id}" class="card shadow">
+            <img class="pin" src="./img/pin.svg" alt="pin"/>
   
-              <div class="debug card-image">
-                  <img src="${element.url}" alt="Photo Number ${element.id}">
-              </div>
+            <div class="debug card-image">
+              <img src="${element.url}" alt="Photo Number ${element.id}">
+            </div>
   
-              <div class="debug card-text">
-                  <p>${element.title}</p>
-              </div>
+            <div class="debug card-text">
+              <p>${element.title}</p>
+            </div>
           </div>
           `;
 
